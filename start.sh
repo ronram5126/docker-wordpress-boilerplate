bdb1f99a196638a0aa8df9fb67733ddd09b8105d
Ronit Ramdam BK <ron.ram5126@gmail.com>
2023-03-12 12:50:37 +0545

    RW bug fix, and added table prefix

--- start.sh
+++ start.sh
@@ -7,13 +7,13 @@ update_project_directories() {
     echo "    volumes:" >> ./docker-compose.override.yml
 
     cd src/plugins
-    for dir in *; do  
-        echo "      - \${PROJECT_SRC_PLUGINS_DIR}/${dir}:\${WP_PLUGINS_DIR}/${dir}" >> ../../docker-compose.override.yml;
+    for dir in $(find . -type d -not -path '*/\.*' -not -path '.' -not -path '..'); do
+        echo "      - \${PROJECT_SRC_PLUGINS_DIR}/${dir#./}:\${WP_PLUGINS_DIR}/${dir#./}" >> ../../docker-compose.override.yml;
     done
 
     cd ../themes
-    for dir in *; do  
-        echo "      - \${PROJECT_SRC_THEMES_DIR}/${dir}:\${WP_THEMES_DIR}/${dir}" >> ../../docker-compose.override.yml;
+    for dir in $(find . -type d -not -path '*/\.*' -not -path '.' -not -path '..'); do
+        echo "      - \${PROJECT_SRC_THEMES_DIR}/${dir#./}:\${WP_THEMES_DIR}/${dir#./}" >> ../../docker-compose.override.yml;
     done
 
     cd ../..
@@ -21,7 +21,7 @@ update_project_directories() {
 
 start_compose() {
     # Making sure compose is down before we start
-    docker-compose down
+    docker-compose stop
     docker-compose up  
 }
 
@@ -29,7 +29,7 @@ exit_trap() {
     echo ""
     echo "Exit Script..";
     echo "=============="
-    docker-compose down
+    docker-compose stop
     # by default these files are unaccessible as these are created by the docker
     # this permission issue will causes problems with git.
     sudo chmod -R 777 ./.docker
